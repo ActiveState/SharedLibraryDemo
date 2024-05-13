899c6bc0a410e3575528e058413e415bf5e11a4c
I-Shen Leong <i-shenl@activestate.com>
2024-05-13 12:24:49 -0700

    Remove version example as it seg faults

--- demo.c
+++ demo.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <unistr.h>
-#include <unistring/version.h>
 
 void main()
 {
-	char name[] = "Testing";
+    char name[] = "Testing";
     printf("libunistring size: %d\n", (int)u8_strlen(name));
-    printf(_LIBUNISTRING_VERSION);
 }
